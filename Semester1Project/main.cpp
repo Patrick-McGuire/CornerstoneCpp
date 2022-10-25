cb6838a46833fd330e8df30dcc0d30a9b6d60d38
Patrick <41267865+Patrick-McGuire@users.noreply.github.com>
2022-10-25 17:59:54 -0400

    stuff

--- Semester1Project/main.cpp
+++ Semester1Project/main.cpp
@@ -9,12 +9,18 @@ using namespace std;
  * Blocking
  */
 void waitToContinue() {
-    char userLet;
+    string userLet;
     cout << "Enter y to continue" << endl;
     cin >> userLet;
-    while (userLet != 'y') {
+    while (userLet != "y" && userLet != "Y") {
         cout << "Invalid Input. Please enter y to continue." << endl;
         cin >> userLet;
+    }
+}
+
+void lower(string &lw) {
+    for(char & i : lw) {
+        i = (char)::tolower(i);
     }
 }
 
@@ -27,6 +33,7 @@ void waitForInput(const string &desiredInput) {
     string inputStr;
     cout << "Please enter the answer." << endl;
     cin >> inputStr;
+    lower(inputStr);
     if (inputStr == desiredInput) {
         cout << "Good Job!" << endl;
     }
@@ -44,7 +51,7 @@ int main() {
         arduino.write("$START\r\n");
     } else {
         std::cout << "Please connect the Arduino" << "\n";
-//        exit(0);
+        exit(0);
     }
 
     // Retrieve the users userName
@@ -53,14 +60,13 @@ int main() {
     cin >> userName;
 
     // Puzzle game intro and instructions
-    cout << "Hello " << userName << "! Welcome to NASA. Your boss is asking you to submit the Executive Committee's response of launching\n"
-                                      "the Colombia Space Shuttle for it's 28th time after it's regular evaluation. With this word hunt,\n"
-                                      "find the shape the words will make (it can be either a square or a hashtag) in order to get\n"
-                                      "the log in information and to find their decision. Please submit it as soon as you can!" << endl;
+    cout << "Hello " << userName << "! Welcome to NASA. The Colombia space shuttle is scheduled to launch later this week.\n"
+                                      "You job is to figure out if it's ready to launch. This would be it's 28th time in space.\n"
+                                      "To determine if it's ready, answer the following questions.\n"
+                                      << endl;
     waitToContinue();
 
-    cout << "A series of fill in the blanks will be asked. Use the answers to find the words in the word hunt and see what\n"
-            "shape is shown! It will increase in difficulty, so good luck!" << endl;
+    cout << "Please complete the following fill in the blank questions! You may need to use google......" << endl;
 
     waitToContinue();
 
@@ -75,98 +81,73 @@ int main() {
     cout << "Dr. Alan _____ is the principal investigator on New Horizons, the first-ever mission to Pluto." << endl;
     waitForInput("stern");
     // Question 4
-    cout << "A _____-Neptunian object, also written transneptunian object, is any minor planet in the Solar System that \n"
+    cout << "A _____-Neptunian object, is any minor planet in the Solar System that \n"
             "orbits the Sun at a greater average distance than Neptune" << endl;
     waitForInput("trans");
 
     // Final part of game
-    cout << "You have correctly found all of the words! Now, what shape does it form in this word hunt?\n"
-            "Here is the word hunt:\n"
+    cout << "Now look for your answers in the following word hunt. Be carefully, they may be backwards!\n"
+            "If the shape created by the answers forms a square, then the shuttle is ready to launch.\n"
             "NATIT\n"
-            "ORBER\n"
-            "VEAKA\n"
-            "AMUTN\n"
+            "RRBER\n"
+            "EWAKA\n"
+            "TMUIN\n"
             "SRATS\n"
-            "Does it form a square or a hashtag?" << endl;
+            "What shape do the answers form?" << endl;
     waitForInput("square");
 
-    // i can make it so it goes back to the top if they get it wrong once
-    cout << "Great! This was the teams decision.\n"
-            "The team has decided to launch the Colombia Space Shuttle for January 16, 2003." << endl;
-
-    waitToContinue();
-
-    string decision;
     cout << "What was the teams decision on launching the Colombia Space Shuttle? Will they (yes) or not (no)?" << endl;
-    cin >> decision;
-    if (decision == "yes") {
-        cout << "That is correct. Thank you for your help." << endl;
-    } else if (decision == "no") {
-        cout << "That is incorrect. Read back on the decision." << endl;
-        cin >> decision;
-    } else {
-        cout << "Invalid Input. Enter 'yes' or 'no'." << endl;
-        cin >> decision;
-    }
+    waitForInput("yes");
 
-    cout << "A few days later";
+    cout << "Thanks! We will be launching in a few days" << endl;
     sleep(1);
-    cout << ".";
-    sleep(1);
-    cout << ".";
-    sleep(1);
-    cout << "." << endl;
-    sleep(1);
+    cout << "A few days later...";
 
-    //Arduino arduino("COM10");
 
-    cout << "News Reporter A: Breaking News! If you look above at the Texas sky, you can see the Colombia Space Shuttle\n "
-            "breaking into multiple pieces while re-entry. NASA is still trying to figure out what occurred, but it \n"
-            "seems to me that something went terribly wrong." << endl;
+    cout << "Breaking News! If you look above at the Texas sky, you can see the Colombia Space Shuttle\n "
+            "breaking into multiple pieces while re-entry. NASA is still trying to figure out what occurred, but\n"
+            "something went terribly wrong." << endl;
     sleep(6); //waits 6 seconds
-    cout << "News Reporter B: We just received news that the 7 astronauts on board have passed away. This is a serious \n"
-            "problem we have here." << endl;
-    sleep(4);
-    cout << "News Reporter A: Come back for more news coverage on this horrible disaster here on-" << endl;
-    sleep(3);
-    cout << "*TV is shut off*" << endl;
+    cout << "The 7 astronauts on board have passed away" << endl;
+
     sleep(2);
-    cout << "What is going on? As an engineer, you need to figure out what went wrong!" << endl;
+    cout << "Your job is to figure out what went wrong." << endl;
     sleep(3);
 
     waitToContinue();
 
-    cout << "Great! Well, not so great. We don't really have much to work of off, but we can start here. Look at the\n"
-            "piece of wood with a missing piece. It seems that this is the main cause for the whole break up of the \n"
-            "shuttle. What could it be? Try to find the missing piece that fits into the hole. " << endl;
+    cout << "We don't really have much to work of off yet, but we can start with the wing. The left\n"
+            "wing has a missing piece. What could it be? This missing piece caused the explosion\n"
+            "Use the pieces in the front of the space shuttle to figure out what caused the issue.\n"
+            "Put the correct piece into the socket on the back of the wing" << endl;
 
-//    waitForInput(arduino, "$CONT2\r\n");
+    waitForInput(arduino, "$CONT2\r\n");
 
-    cout << "Good job! You found the right piece. What do you think it is? This is a heat plate! It looks\n"
-            " like its damaged too... " << endl;
+    cout << "Good job! You found the right piece. This is a heat plate! Losing heat tiles has \n"
+            "been a big issue in NASA for many years, with 2,000 heat tiles being lost while on an airplane for Colombia,\n"
+            "along with 15 tiles being lost during blast off for the shuttle alone." << endl;
     sleep(3);
 
     waitToContinue();
 
-    cout << "Here, we can see that the heat tile was damaged, causing for the heat to flow inside\n"
-            "the space shuttle. This heat tile was located on the left wing of the shuttle. With the high velocity and heat,\n"
-            "it caused the shuttle to break up, leaving only remains of the space shuttle such as this heat tile. \n"
-            "Now, we need to find what caused this heat tile to break." << endl;
+    cout << "The damaged heat tile caused the heat to flow inside the space shuttle. With the high velocity and heat, it\n"
+            "caused the shuttle to break up, leaving only remains of the space shuttle. But what damaged the heat tile?\n" << endl;
     sleep(3);
 
     waitToContinue();
 
-    cout << "Move to the space shuttle itself now. We need to locate what piece hit the heat tile and caused it to fall\n"
-            "off. Here are multiple pieces of junk that are possible choices for it to hit the heat tile, and cause the\n"
-            "disaster. Try pulling each one and find the one that broke the shape shuttle. " << endl;
+    cout << "You need to locate what piece hit the heat tile and caused it to damage it, eventually causing it to fall off. There is \n"
+            "a loose object on the space shuttle that caused the damaged heat tile. Find that loose object and pull it."<< endl;
     sleep(3);
 
+    std::cout << "Continue after you have found the loose object" << std::endl;
     waitToContinue();
 
-    cout << "Woah! This is the piece of thing that caused the entire thing to break. This piece of foam was the key reason\n"
-            "as to why the entire shuttle broke down during re-entry. Even though the Committee decide to launch the space shuttle\n"
-            "they knew about this problem even before-hand. Serious consequences were faced from those who allowed this\n"
-            "disaster to occur." << endl;
+    cout << "The loose object was a piece of insulating foam. This was the key reason why the entire shuttle \n"
+            "broke down during re-entry. Even though they knew about problems similar to this, the Committee decide to launch the space shuttle. \n"
+            " In this case, NASA found that the piece of foam has damaged the shuttle a day after the launch, but the Engineers \n"
+            "came to the conclusion that the problem was not vital enough, and ignored it. NASA knew that there could be a chain reaction after \n"
+            "if one tile got damaged. but proceeded anyway. This negligence cost 7 people's lives and shows how careful engineers have to be when dealing with human life." << endl;
     sleep(3);
 
     waitToContinue();
