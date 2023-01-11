6b04c092b646ecdf40e504490c2cbe8725754731
Patrick <41267865+Patrick-McGuire@users.noreply.github.com>
2023-01-11 13:43:18 -0500

    Added array assigment

--- FlagVariable/main.cpp
+++ FlagVariable/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <optional>
 #include <vector>
+#include <cstring>
 #include "ConfigVariable.h"
 
 using std::cout;
 
 const ConfigFlag EXIST_MAX = ConfigFlag(EXISTS | MAX);
 
+void reverse(char *str) {
+    for(int i = 0; i < strlen(str) / 2; i++) {
+        char sw = str[i];
+
+    }
+}
+
 int main() {
-
-    ConfigVariable<int> a;
-
-//    a.addFlag(EXISTS);
-    a.addFlag(MIN);
-    a.addFlag(MAX);
-    a.addFlag(AUTO);
-
-    cout << a.checkFlag(EXIST_MAX) << "\n";
+    char *a = "1234";
+//    ConfigVariable<int> a;
+//
+////    a.addFlag(EXISTS);
+//    a.addFlag(MIN);
+//    a.addFlag(MAX);
+//    a.addFlag(AUTO);
+//
+//    cout << a.checkFlag(EXIST_MAX) << "\n";
 
 //    std::vector<ConfigFlag> lst = {EXISTS, MAX, MIN, AUTO};
 //    for(auto el : lst) {
